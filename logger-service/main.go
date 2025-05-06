637a17a411dfb6bd9936029dddbbb1591b8c9212
Robert076 <robertberes06@gmail.com>
2025-05-06 15:09:15 +0300

    working, remember the image pull policy for k8s

--- logger-service/main.go
+++ logger-service/main.go
@@ -11,20 +11,24 @@ import (
 )
 
 func main() {
+
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			http.Error(w, "Only POST method is allowed on this endpoint.", http.StatusMethodNotAllowed)
+			log.Print("Only POST method is allowed on this endpoint.")
 			return
 		}
 
 		var newPing model.Ping
 		if err := json.NewDecoder(r.Body).Decode(&newPing); err != nil {
 			http.Error(w, "Invalid JSON format.", http.StatusBadRequest)
+			log.Print(err)
 			return
 		}
 
 		if newPing.Message == "" {
 			http.Error(w, "Ping message cannot be empty.", http.StatusBadRequest)
+			log.Print("Message cannot be empty")
 			return
 		}
 
@@ -32,20 +36,22 @@ func main() {
 
 		if err != nil {
 			http.Error(w, "Error inserting ping into database.", http.StatusInternalServerError)
+			log.Print(err)
 			return
 		}
 
 		w.WriteHeader(http.StatusOK)
 		if err := json.NewEncoder(w).Encode(id); err != nil {
 			http.Error(w, "Error encoding new id.", http.StatusInternalServerError)
+			log.Print(err)
+			return
 		}
 	})
 
-	log.Println("Hello world!")
-	log.Println("Hello world!2")
 	http.HandleFunc("/pings", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
 			http.Error(w, "Only GET method is allowed on this endpoint.", http.StatusMethodNotAllowed)
+			log.Print("Only GET method is allowed on this endpoint.")
 			return
 		}
 
@@ -55,17 +61,19 @@ func main() {
 		fmt.Println(err)
 		if err != nil {
 			http.Error(w, "Error retrieving pings from database.", http.StatusInternalServerError)
+			log.Print(err)
 			return
 		}
 
 		if err := json.NewEncoder(w).Encode(pings); err != nil {
 			http.Error(w, "Failed to encode pings", http.StatusInternalServerError)
+			log.Print(err)
 			return
 		}
 	})
 
 	if err := http.ListenAndServe(":1234", nil); err != nil {
-		log.Fatalf("Cannot start server: %v", err)
+		log.Printf("Cannot start server: %v", err)
 		return
 	}
 }
