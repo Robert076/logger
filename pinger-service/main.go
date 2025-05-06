fdee1790d46d3f15a064f9c2cb06ae6c64dfcdf1
Robert076 <robertberes06@gmail.com>
2025-05-06 11:13:23 +0300

    hardcoded messages

--- pinger-service/main.go
+++ pinger-service/main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"fmt"
+
+	data "github.com/Robert076/logger/pinger-service/messages"
+)
+
+func main() {
+	fmt.Print(data.Messages)
+}
---
+++ pinger-service/messages/messages.go
@@ -0,0 +1,24 @@
+package messages
+
+var Messages = []string{
+	"Why does bread always land butter side down?",
+	"Is cereal soup or a salad?",
+	"Do penguins have knees?",
+	"How much wood would a woodchuck chuck?",
+	"Why do we say 'sleep like a baby' when babies wake up every two hours?",
+	"Can you sneeze with your eyes open?",
+	"Do fish get thirsty?",
+	"Why are round pizzas delivered in square boxes?",
+	"Is it possible to cry underwater?",
+	"Do we ever actually use the phrase 'break a leg' in a real situation?",
+	"Why do we call it a 'building' when it's already built?",
+	"Is a hot dog a sandwich?",
+	"Can a tornado destroy a tornado?",
+	"Do shadows have weight?",
+	"If we weren’t meant to eat animals, why are they made of meat?",
+	"What would happen if you painted your house with invisible paint?",
+	"If a tree falls in the forest and nobody’s around, does it still make a sound?",
+	"Why is 'abbreviated' such a long word?",
+	"Would you ever eat a cookie that could talk?",
+	"If aliens exist, why haven’t they sent us a text yet?",
+}
